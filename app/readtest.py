35ae0ab2da331ca3cbd15be017bc4a0521272e94
pboavida <pedrom2boavida@gmail.com>
2020-02-18 14:26:09 +0200

    testei

---
+++ app/readtest.py
@@ -0,0 +1,96 @@
+import requests
+import json
+from os import getenv
+
+
+
+
+
+base_url = 'https://immoscout-api-ji3l2ohvha-lz.a.run.app'
+
+summary_url = '/get_summary'
+page_url = '/get_list?page='
+data_url = '/get_data?id='
+
+get_summary = requests.get(base_url + summary_url,headers={"accept":"application/json","X-API-KEY":"dffbab93-44e9-41c2-bfff-6bab66c89b6c"})
+if get_summary:
+    print('Success!')
+else:
+    print('An error has occurred getting the data summary.')
+
+
+
+total_pages = get_summary.json()["total_pages"]
+total_adds = get_summary.json()["total_ads"]
+
+
+i = 2
+page_info = []
+id_list = []
+ad_list = []
+
+temp_list = requests.get(base_url + page_url + str(i),headers={"accept": "application/json", "X-API-KEY": "dffbab93-44e9-41c2-bfff-6bab66c89b6c"})
+if temp_list:
+    print(temp_list.json())
+    page_info.append(temp_list.json())
+    x = page_info[0]['ids'][2]
+
+    temp_data= requests.get(base_url + data_url + str(x),headers={"accept": "application/json", "X-API-KEY": "dffbab93-44e9-41c2-bfff-6bab66c89b6c"})
+
+
+
+
+print('immoscout_id:=',temp_data.json()['expose.expose']['realEstate']['@id'])
+print('area_sq_m:=',temp_data.json()['expose.expose']['realEstate']['livingSpace'])
+print('cnt_rooms:=',temp_data.json()['expose.expose']['realEstate']['numberOfRooms'])
+print('cnt_floors:=',temp_data.json()['expose.expose']['realEstate']['numberOfFloors'])
+print('floor:=',temp_data.json()['expose.expose']['realEstate']['floor'])
+print('type=',temp_data.json()['expose.expose']['realEstate']['apartmentType'])
+print('has_fitted_kitchen:=',temp_data.json()['expose.expose']['realEstate']['builtInKitchen'])
+print('has_lift:=',temp_data.json()['expose.expose']['realEstate']['lift'])
+print('has_balcony:=',temp_data.json()['expose.expose']['realEstate']['balcony'])
+print('has_garden:=',temp_data.json()['expose.expose']['realEstate']['garden'])
+print('has_guest_toilet:=',temp_data.json()['expose.expose']['realEstate']['guestToilet'])
+print('is_barrier_free:=',temp_data.json()['expose.expose']['realEstate']['handicappedAccessible'])
+if 'heatingType' in temp_data.json()['expose.expose']['realEstate']:
+    print('heating_type:=',temp_data.json()['expose.expose']['realEstate']['heatingType'])
+if 'thermalCharacteristic' in temp_data.json()['expose.expose']['realEstate']:
+    print('thermal_characteristic:=',temp_data.json()['expose.expose']['realEstate'])
+print('total_rent:=',temp_data.json()['expose.expose']['realEstate']['totalRent'])
+print('calculatedTotalRent=',temp_data.json()['expose.expose']['realEstate']['calculatedTotalRent'])
+print('base_rent:=',temp_data.json()['expose.expose']['realEstate']['baseRent'])
+print('service_charge:=',temp_data.json()['expose.expose']['realEstate']['serviceCharge'])
+print('deposit:=',temp_data.json()['expose.expose']['realEstate']['deposit'])
+print('city:=',temp_data.json()['expose.expose']['realEstate']['address']['city'])
+print('district:=',temp_data.json()['expose.expose']['realEstate']['address']['quarter'])
+print('zip_code:=',temp_data.json()['expose.expose']['realEstate']['address']['postcode'])
+print('street:=',temp_data.json()['expose.expose']['realEstate']['address']['street'])
+print('house_number:=',temp_data.json()['expose.expose']['realEstate']['address']['houseNumber'])
+print('lng:=',temp_data.json()['expose.expose']['realEstate']['address']['wgs84Coordinate']['longitude'])
+print('lat:=',temp_data.json()['expose.expose']['realEstate']['address']['wgs84Coordinate']['latitude'])
+print('company_name:=',temp_data.json()['expose.expose']['contactDetails']['company'])
+print('contact_firstname:=',temp_data.json()['expose.expose']['contactDetails']['firstname'])
+print('contact_lastname:=',temp_data.json()['expose.expose']['contactDetails']['lastname'])
+print('salutation:=',temp_data.json()['expose.expose']['contactDetails']['salutation'])
+print('email:=',temp_data.json()['expose.expose']['contactDetails']['email'])
+
+print('phone_number:=',temp_data.json()['expose.expose']['contactDetails']['phoneNumberCountryCode'])
+print('phone_number:=',temp_data.json()['expose.expose']['contactDetails']['phoneNumberAreaCode'])
+print('phone_number:=',temp_data.json()['expose.expose']['contactDetails']['phoneNumberSubscriber'])
+print('phone_number:=',temp_data.json()['expose.expose']['contactDetails']['phoneNumber'])
+print('mobile_number:=',temp_data.json()['expose.expose']['contactDetails']['cellPhoneNumber'])
+print('address_city:=',temp_data.json()['expose.expose']['contactDetails']['address']['city'])
+print('address_street:=',temp_data.json()['expose.expose']['contactDetails']['address']['street'])
+print('address_zip_code:=',temp_data.json()['expose.expose']['contactDetails']['address']['postcode'])
+print('address_house_number:=',temp_data.json()['expose.expose']['contactDetails']['address']['houseNumber'])
+
+
+
+
+
+
+
+
+
+
+
